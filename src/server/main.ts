48583573ae611de30f6b3ad8c970297451875634
nico-series <nico.macisaac@gmail.com>
2025-10-19 20:00:36 -0400

    modified package and config presets

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+const PORT = process.env.PORT || 3000;
+// i understand that app is usually lowercase but if it is a const right next to other consts this is painful
 const app = express();
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
+ViteExpress.listen(app, +PORT, () =>
   console.log("Server is listening on port 3000..."),
 );

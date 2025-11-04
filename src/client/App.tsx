83474ab17e2cfb5d743a141fa15d3010b59a84a5
nico-series <nico.macisaac@gmail.com>
2025-11-04 18:33:38 -0500

    added vitest, brainstorming, select skeleton

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,23 +1,14 @@
 import "./App.css";
 
-import { useState } from "react";
+import ProdGoalsForm from "./components/ProdGoalsForm.js";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Wannabe</h1>
       <p>Another Factorio tool</p>
-      <p>Given a production goal, the form should return the necessary build requirements.</p>
-      <form>
-        <label htmlFor="goal">Production Goal:
-          <input type="text" id="goal"></input>
-        </label>
-        <label htmlFor="quantity">Quantity: 
-          <input type="number" id="quantity"></input>
-        </label>
-      </form>
+
+      <ProdGoalsForm />
     </div>
   );
 }
---
+++ src/client/components/ProdGoalsForm.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest';
+
+// brainstorming relevant tests
+// a lot of these are going to require refinement
+// any validation should be verified
+describe('goal input', () => {
+    test('should populate a list', () => {
+
+    })
+
+    test('should complain about entry of unlisted values', () => {
+
+    })
+})
+
+describe('quantity input', () => {
+    // wym
+    test('should complain about invalid quantities', () => {
+
+    })
+})
+
+describe('form', () => {
+    test('should submit with valid data', () => {
+
+    })
+})
